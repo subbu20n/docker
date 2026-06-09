52792b29e47f50bccc8069153887a08b40d25764
subbu <subbunandyala48@gmail.com>
2026-06-09 15:08:34 +0530

    docker

--- docker.tf
+++ docker.tf
@@ -4,12 +4,12 @@ resource "aws_instance" "docker" {
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
 
   #need more for terraform  
-  root_block_device {
-    volume_size = 50 
-    volume_type = "gp3" # or gpe depending on your preference 
-  }
+  # root_block_device {
+  #   volume_size = 50 
+  #   volume_type = "gp3" # or gpe depending on your preference 
+  # }
 
-  user_data = file("docker.sh") 
+  # user_data = file("docker.sh") 
   #iam_instance_profile = "TerraformAdmin" 
 
   tags = {
