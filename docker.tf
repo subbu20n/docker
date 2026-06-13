870bda2d6ea5f66236c71e8743a538c4639cee92
subbu <subbunandyala48@gmail.com>
2026-06-13 13:23:22 +0530

    docker

--- docker.tf
+++ docker.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "docker" {
   ami = local.ami_id 
-  instance_type = "t3.medium"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
 
   #need more for terraform  
-  root_block_device {
-    volume_size = 50 
-    volume_type = "gp3" # or gpe depending on your preference 
-  }
+  # root_block_device {
+  #   volume_size = 50 
+  #   volume_type = "gp3" # or gpe depending on your preference 
+  # }
 
   # user_data = file("docker.sh") 
   #iam_instance_profile = "TerraformAdmin" 
